bdaa0bfa23d84607bab06017140d8df27d49a56d
Jan van Hest <jan@vanhest.work>
2019-06-05 15:58:04 +0200

    Added HTML elemets

--- index.php
+++ index.php
@@ -1,22 +1,61 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap-grid.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap-grid.min.css" crossorigin="anonymous">
 
     <title>Hello, world!</title>
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
+</head>
+
+<body class="bg-light">
+    <div class="container">
+        <div class="row">
+
+            <!-- Start of form -->
+            <h1>Inschrijfformulier</h1>
+        </div>
+
+        <!-- Adresgegevens client -->
+        <div class="row">
+            <div class="col">
+
+                <h2>Naamgegegevens</h2>
+                <p><label for="">Voornaam:</label><input type="text" class="form-control" id="firstName" placeholder="" value="" required=""></p>
+                <p><label for="">Achternaam:</label><input type="text" class="form-control" id="firstName" placeholder="" value="" required=""></p>
+                <p>Geboortedatum:</p>
+                <h2>Adresgegevens</h2>
+                <p><label for="">Straatnaam, nummer</label><input type="text" class="form-control" id="firstName" placeholder="" value="" required=""></p>
+                <p>Postcode,
+                    <label for="">Plaatsnaam.</label><input type="text" class="form-control" id="firstName" placeholder="" value="" required=""></p>
+                <h2>Contactgegevens:</h2>
+                <p>Mobile number:</p>
+                <p>Mail address:</p>
+            </div>
+
+            <div class="col">
+                <h2>Persoonlijke gegevens:</h2>
+                <label for="">Reden van aanmelding</label><input type="text" class="form-control" id="firstName" placeholder="" value="" required="">
+                <!-- tekst vak wat vrij in te vullen is. -->
+                <p>Rooster</p>
+                <!-- checkmark vakjes met maandag, dinsdag, woensdag, donderdag, vrijdag. Ochtend en middag apart aan te vinken. -->
+                <p>Opmerkingen</p>
+                <!-- tekst vak wat vrij in te vullen is. -->
+                <p> Verstuur knop om het formulier in te zenden.</p>
+            </div>
+        </div>
+    </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="node_modules/jquery/dist/jquery.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="node_modules/popper.js/dist/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="node_modules/bootstrap/dist/css/bootstrap.min.css" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+    <script src="node_modules/jquery/dist/jquery.slim.min.js" crossorigin="anonymous"></script>
+    <script src="node_modules/popper.js/dist/popper.min.js" crossorigin="anonymous"></script>
+    <script src="node_modules/bootstrap/dist/css/bootstrap.min.css" crossorigin="anonymous"></script>
+</body>
+</div>
+
 </html>
