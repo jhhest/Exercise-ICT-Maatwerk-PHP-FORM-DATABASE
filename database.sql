5ce062aeaf3d49063f6847b7849d0b7f0bc073a3
Jan van Hest <jan@vanhest.work>
2019-06-18 10:13:05 +0200

    Added input for mail

---
+++ database.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE ict_maatwerk;
+USE DATABASE ict_maatwerk;
+CREATE TABLE clients(
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  voornaam,
+  achternaam,
+  geboorte_datum,
+  vestiging,
+  mobiel nummer,
+  mailadresadres,
+);
+CREATE TABLE schedule(
+  id,
+  maandag,
+  dinsdag,
+  woensdag,
+  donderdag,
+  vrijdag,
+  zaterdag,
+  zondag,
+);
