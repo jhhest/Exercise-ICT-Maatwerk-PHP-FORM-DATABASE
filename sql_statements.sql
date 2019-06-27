4cb0a708a5e52125be30bc3f6d66ec53235bae53
Jan van Hest <jan@vanhest.work>
2019-06-28 00:02:21 +0200

    Updates SQL Statements
    Removed table contact,
    Removed table adresses, 
    updated incorrect foreign key's. Now all foreign keys point in the right direction.

--- sql_statements.sql
+++ sql_statements.sql
@@ -1,59 +1,50 @@
+CREATE DATABASE ICT_maatwerk_form;
+
 CREATE TABLE `klanten` (
-  `klant_id` INT NOT NULL AUTO_INCREMENT,
-  `voornaam` VARCHAR(255) NOT NULL,
-  `achternaam` VARCHAR(255) NOT NULL,
-  `geboortedatum` DATE NOT NULL,
-  `adres` VARCHAR(255) NOT NULL,
-  `opmerkingen` TEXT NOT NULL,
-  `rooster` TEXT NOT NULL,
-  `Mobiele_telefoon` INT NOT NULL,
-  `Reden van aanmelding` TEXT NOT NULL,
-  PRIMARY KEY (`klant_id`)
+	`klant_id` INT NOT NULL AUTO_INCREMENT,
+	`voornaam` VARCHAR(255) NOT NULL,
+	`achternaam` VARCHAR(255) NOT NULL,
+	`geboortedatum` DATE NOT NULL,
+	`adres` VARCHAR(255) NOT NULL,
+	`postcode` varchar(255) NOT NULL,
+	`plaats` varchar(255) NOT NULL,
+	`opmerkingen` TEXT NOT NULL,
+	`rooster` TEXT NOT NULL,
+	`Mobiele_telefoon` INT NOT NULL,
+	`Reden van aanmelding` TEXT NOT NULL,
+	PRIMARY KEY (`klant_id`)
 );
-CREATE TABLE `locatie` (
-  `locatie_id` INT NOT NULL AUTO_INCREMENT,
-  `adres` VARCHAR(255) NOT NULL,
-  `Postcode` varchar(255) NOT NULL,
-  `woonplaats` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`locatie_id`)
-);
+
 CREATE TABLE `Roostertijden` (
-  `rooster_ID` INT NOT NULL AUTO_INCREMENT,
-  `dagdelen` INT NOT NULL,
-  PRIMARY KEY (`rooster_ID`)
+	`rooster_ID` INT NOT NULL AUTO_INCREMENT,
+	`dagdelen` INT NOT NULL,
+	PRIMARY KEY (`rooster_ID`)
 );
+
 CREATE TABLE `dagdelen` (
-  `aanwezigheid_id` BOOLEAN NOT NULL AUTO_INCREMENT DEFAULT false,
-  `Maandagochtend` BOOLEAN NOT NULL DEFAULT false,
-  `Maandagmiddag` BOOLEAN NOT NULL DEFAULT false,
-  `maandagvrij` BOOLEAN NOT NULL DEFAULT false,
-  `dinsdagochtend` BOOLEAN NOT NULL DEFAULT false,
-  `dinsdagmiddag` BOOLEAN NOT NULL DEFAULT false,
-  `dinsdagvrij` BOOLEAN NOT NULL DEFAULT false,
-  `woensdagochtend` BOOLEAN NOT NULL DEFAULT false,
-  `woensdagmiddag` BOOLEAN NOT NULL DEFAULT false,
-  `woensdagvrij` BOOLEAN NOT NULL DEFAULT false,
-  `donderdagochtend` BOOLEAN NOT NULL DEFAULT false,
-  `donderdagmiddag` BOOLEAN NOT NULL DEFAULT false,
-  `donderdagvrij` BOOLEAN NOT NULL DEFAULT false,
-  `vrijdagochtend` BOOLEAN NOT NULL DEFAULT false,
-  `vrijdagmiddag` BOOLEAN NOT NULL DEFAULT false,
-  `vrijdagvrij` BOOLEAN NOT NULL DEFAULT false,
-  PRIMARY KEY (`aanwezigheid_id`)
+	`aanwezigheid_id` INT NOT NULL AUTO_INCREMENT,
+	`Maandagochtend` BOOLEAN NOT NULL DEFAULT false,
+	`Maandagmiddag` BOOLEAN NOT NULL DEFAULT false,
+	`maandagvrij` BOOLEAN NOT NULL DEFAULT false,
+	`dinsdagochtend` BOOLEAN NOT NULL DEFAULT false,
+	`dinsdagmiddag` BOOLEAN NOT NULL DEFAULT false,
+	`dinsdagvrij` BOOLEAN NOT NULL DEFAULT false,
+	`woensdagochtend` BOOLEAN NOT NULL DEFAULT false,
+	`woensdagmiddag` BOOLEAN NOT NULL DEFAULT false,
+	`woensdagvrij` BOOLEAN NOT NULL DEFAULT false,
+	`donderdagochtend` BOOLEAN NOT NULL DEFAULT false,
+	`donderdagmiddag` BOOLEAN NOT NULL DEFAULT false,
+	`donderdagvrij` BOOLEAN NOT NULL DEFAULT false,
+	`vrijdagochtend` BOOLEAN NOT NULL DEFAULT false,
+	`vrijdagmiddag` BOOLEAN NOT NULL DEFAULT false,
+	`vrijdagvrij` BOOLEAN NOT NULL DEFAULT false,
+	PRIMARY KEY (`aanwezigheid_id`)
 );
-ALTER TABLE
-  `klanten`
-ADD
-  CONSTRAINT `klanten_fk0` FOREIGN KEY (`adres`) REFERENCES `locatie`(`locatie_id`);
-ALTER TABLE
-  `klanten`
-ADD
-  CONSTRAINT `klanten_fk1` FOREIGN KEY (`opmerkingen`) REFERENCES ``(``);
-ALTER TABLE
-  `klanten`
-ADD
-  CONSTRAINT `klanten_fk2` FOREIGN KEY (`rooster`) REFERENCES `Roostertijden`(`rooster_ID`);
-ALTER TABLE
-  `Roostertijden`
-ADD
-  CONSTRAINT `Roostertijden_fk0` FOREIGN KEY (`dagdelen`) REFERENCES `dagdelen`(`aanwezigheid_id`);
+
+ALTER TABLE `klanten` ADD CONSTRAINT `klanten_fk0` FOREIGN KEY (`adres`) REFERENCES `locatie`(`locatie_id`);
+
+ALTER TABLE `klanten` ADD CONSTRAINT `klanten_fk1` FOREIGN KEY (`opmerkingen`) REFERENCES ``(``);
+
+ALTER TABLE `klanten` ADD CONSTRAINT `klanten_fk2` FOREIGN KEY (`rooster`) REFERENCES `Roostertijden`(`rooster_ID`);
+
+ALTER TABLE `Roostertijden` ADD CONSTRAINT `Roostertijden_fk0` FOREIGN KEY (`dagdelen`) REFERENCES `dagdelen`(`aanwezigheid_id`);
