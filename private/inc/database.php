93a0f87ec2a6276dd8358b253d01df95ae990f6a
Jan van Hest <jan@vanhest.work>
2019-07-05 10:28:47 +0200

    Updated private/inc/database.php. Added functions to connect to database
    * added database guide form linkedin.com/learning
    * added database credentials as constants
    * added function db_connect
    * added function db_disconnect
    
     use $db = db_connect(); everywhere you need to make a connection to the database.

--- private/inc/database.php
+++ private/inc/database.php
@@ -1,40 +1,101 @@
 <?php
 
 /***************************************************
- * 
- * Setup Database connection. 
- *  
- * CRUD
- * 
- * C Create
- * R Read
- * U Update
- * D Delete
- * 
- * mysql --version
- * * Check mysql version
- * 
- * mysql -u root(depends on installation if you have a password needed )
- * * Login 
- * 
- * mysql -u root -p
- *  * Set a password 
- * 
- * SHOW DATABASES;
- * CREATE DATABASE db_name;
- * USE db_name
- * DROP DATABASE db_name;
- *
- * Make a user with specific acces to a specific user. Limits the scope of acces to mysql to your databases. 
- * 
- * GRANT ALL PRIVILEGES ON db_name.*
- * TO 'username'@'localhost'
- * IDENTIFIED BY 'password';
- * 
- * SHOW GRANTS FOR 'username'@'localhost'
- *  
- */
+ * Setup Database connection.
+ *************************************************** 
+  
+CRUD
+ 
+C Create
+R Read
+U Update
+D Delete
+
+mysql --version
+ * Check mysql version
+
+mysql -u root(depends on installation if you have a password needed )
+ * Login 
+
+mysql -u root -p
+ * Set a password 
+
+SHOW DATABASES;
+CREATE DATABASE db_name;
+USE db_name
+DROP DATABASE db_name;
+Make a user with specific acces to a specific user. Limits the scope of acces to mysql to your databases. 
+
+GRANT ALL PRIVILEGES ON db_name.*
+TO 'username'@'localhost'
+IDENTIFIED BY 'password';
+
+SHOW GRANTS FOR 'username'@'localhost'
+ 
+
+/**************************************************************
+ * DB Guide Linkedin learning
+ **************************************************************
+
+<?php
+
+// This guide demonstrates the five fundamental steps
+// of database interaction using PHP.
+
+// Credentials
+$dbhost = '';
+$dbuser = '';
+$dbpass = '';
+$dbname = '';
+
+// 1. Create a database connection
+$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+// 2. Perform database query
+
+// 3. Use returned data (if any)
+
+// 4. Release returned data
+
+// 5. Close database connection
+mysqli_close($connection);
+
 echo "<p>remark: Start of database.php</p>"
+
+ */
+
+/****************************************************************
+ * db credentials
+ */
+$dbhost = 'localhost';
+$dbuser = 'jasper';
+$dbpass = 'REAcollegeVpro';
+$dbname = 'ICT_maatwerk_aanmeldformulier';
+
+$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+define("DB_SERVER", "localhost");
+define("DB_USER", "jasper");
+define("DB_PASS", "REAcollegeVpro");
+define("DB_NAME", "ICT_maatwerk_aanmeldformulier");
+
+function db_connect() {
+    $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+}
+
+function db_disconnect($connection){
+    if(isset($connection)) { 
+        mysqli_close($connection);
+    }
+}
+
+/****
+ * 
+ * use $db = db_connect(); everywhere you need to make a connection to the database.  
+ *  
+ * */ 
 
 
 ?>
+
+
